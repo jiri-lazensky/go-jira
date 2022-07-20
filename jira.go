19bec0ae42cd7bc39340ae4791e89860bfb227e7
jiri.lazensky <>
2022-07-20 23:12:37 +0200

    feat: add debug for outgoing json

--- jira.go
+++ jira.go
@@ -179,6 +179,10 @@ func (c *Client) NewRequestWithContext(ctx context.Context, method, urlStr strin
 		}
 	}
 
+	// DEBUG FORTUNA
+	fmt.Println(time.Now(), "transition payload - body", body)
+	fmt.Println(time.Now(), "transition payload - buffer", buf)
+
 	req, err := newRequestWithContext(ctx, method, u.String(), buf)
 	if err != nil {
 		return nil, err
